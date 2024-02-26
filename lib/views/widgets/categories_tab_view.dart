e4a4c5a5333cdbfbcb8a27c7b500827a9f7d5ac2
DaanaHA <danaheyam2@gmail.com>
2024-02-26 19:01:31 +0200

    more classes

--- lib/views/widgets/categories_tab_view.dart
+++ lib/views/widgets/categories_tab_view.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
- 
- class CategoriesTabView extends StatelessWidget {
-   const CategoriesTabView({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return const Column(
-      children: [],
-     );
-   }
- }
+
+class CategoriesTabView extends StatelessWidget {
+  const CategoriesTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Categories Tab View'),);
+  }
+}
